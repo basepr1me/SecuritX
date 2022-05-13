3e303e790346e93cb11f02b6bf2e66b84fc4ede9
Tracey Emery <tracey@traceyemery.net>
2022-05-13 22:52:55 +0000

    don't overwrite member data

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -15,7 +15,7 @@
 return [
 	/* edit this */
 	'email_host' => [
-		'hostname'	=> 'securitx.localhost',
+		'hostname'	=> 'securitx.traceyemery.com',
 		'ip'		=> '127.0.0.1',
 	],
 
@@ -25,8 +25,8 @@ return [
 		'dsn'		=> sprintf('sqlite:%s/data/securitx.db', realpath(getcwd())),
 	],
 	'recaptcha' => [
-		'site_key' => "enter recaptcha site key",
-		'secret_key' => "enter recaptcha secret key",
+		'site_key' => "6Lf_bHkfAAAAAK13SmqVWW-f36tS_GfAEs0pJa6p",
+		'secret_key' => "6Lf_bHkfAAAAAGhrZdsSt0-yvJWT9AZf9VYi48lR",
 	],
 	'hipaa' => [
 		'notice' => 'DISCLOSURE: This message is intended only for the use of the individual(s) to whom it is addressed and contains information that is privileged, confidential, and exempt from disclosure under applicable law. Any further dissemination or copying of this communication is strictly prohibited. If you have received this communication in error, please notify us immediately by telephone or email as listed in our signature above. This message is provided in accordance with HIPAA Omnibus Rule of 2013.',
